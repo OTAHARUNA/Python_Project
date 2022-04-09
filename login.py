f9f8ffd1da01307dc6c5360f73ade1b6c4169e03
OTAHARUNA <radwimps.love867@gmail.com>
2022-04-09 19:28:31 +0900

    予約画面三つ目のリスト画面開く処理まで。

--- login.py
+++ login.py
@@ -10,29 +10,33 @@ driver = webdriver.Chrome(executable_path=cf.CHROMEDRIVER)
 # Googleの検索TOP画面を開く。
 driver.get(cf.URL)
 driver.maximize_window()
-driver.save_screenshot('ログイン画面.png')
-time.sleep(3)
+driver.save_screenshot('./images_evidence/ログイン画面.png')
+time.sleep(1)
 
 # ログインIDを入力
 login_email = driver.find_element_by_name("email")
 login_email.send_keys(cf.email)
+time.sleep(1)
 # パスワードを入力
 password = driver.find_element_by_name("password")
 password.send_keys(cf.password)
+time.sleep(1)
+
 #ログインボタンをクリック
 login_btn = driver.find_element_by_xpath('//*[@class="form-group"]/form/button')
 login_btn.click()
-time.sleep(3)
+time.sleep(1)
 
-# スクール予約確認画面へ遷移
+# 閉じるボタン押下
 close_btn = driver.find_element_by_xpath('//*[@class="modal-footer"]/button')
 close_btn.click()
+
 # スクール予約確認画面へ遷移
-reserve_btn = driver.find_element_by_xpath('//*[@id="side-bar"]/ul/li/a')
+reserve_btn = driver.find_element_by_xpath('//*[@id="side-bar"]/ul/li[3]/a')
 reserve_btn.click()
 
 driver.maximize_window()
-driver.save_screenshot('予約画面.png')
+driver.save_screenshot('./images_evidence/予約画面.png')
 
 #10秒待機
 time.sleep(10)
