a500bc60b840550df57039dd8a9680bcabbc405c
OTAHARUNA <radwimps.love867@gmail.com>
2022-04-17 21:11:40 +0900

    日本語含むパス対応

--- excel.py
+++ excel.py
@@ -1,8 +1,10 @@
+# coding:utf-8
 import os
 import glob
 import imghdr
 import openpyxl
 import cv2
+import numpy as np
 
 
 # 定数設定
@@ -36,7 +38,9 @@ def attach_img(target_full_file_names, set_column_idx, set_dir_name):
             img = openpyxl.drawing.image.Image(target_file)
 
             # 画像のサイズを取得して、セルの大きさを合わせる（画像同士が重ならないようにするため）
-            size_img = cv2.imread(target_file)
+            size_img = np.fromfile(target_file, dtype=np.uint8)
+            size_img = cv2.imdecode(size_img, cv2.IMREAD_COLOR)
+            # size_img = cv2.imread(target_file) #このままだと日本語含むパスに対応していない
             height, width = size_img.shape[:2]
 
             img.width = 944 #25cmの事
@@ -45,11 +49,11 @@ def attach_img(target_full_file_names, set_column_idx, set_dir_name):
             if max_width < img.width:
                 max_width = img.width
             if not max_height[set_row_idx-1:set_row_idx]: # 配列「max_height」において、「set_row_idx」番目の要素が存在しなければ、挿入
-                max_height.insert(set_row_idx-1, height)
-            if max_height[set_row_idx-1] < height:
-                max_height[set_row_idx-1] = height
+                max_height.insert(set_row_idx-1, img.height)
+            if max_height[set_row_idx-1] < img.height:
+                max_height[set_row_idx-1] = img.height
 
-            column = str(column_letter)
+            # column = str(column_letter)
             ws.row_dimensions[set_row_idx+1].height = max_height[set_row_idx-1] * 0.75
             ws.column_dimensions['A'].width = max_width * 0.13
             # ws.column_dimensions[column_letter].width = max_width * 0.13
--- python.md
+++ python.md
@@ -36,3 +36,32 @@ for no in range(1,4):
     driver.save_screenshot('./images_evidence/予約画面' + str(no) + '.png')
 #特に終わりの宣言は不要でforの中身はインデント下げて、抜け出すときはインデント同じ
 ```
+### 文字コード
+文字コード設定はファイルの頭で行わないとデフォルトのUTF-8になる
+```python
+# coding:utf-8
+```
+
+### 日本語含むファイル名
+OpenCVでは日本語含むファイルパスには対応していない。
+PILを使用するか、Numpyを使用するかになる。
+PILを使用するにしても最終的にはNumpyを使用しているのでNumpyがいいとのこと。
+
+```python
+# PIL
+import cv2
+import numpy as np
+from PIL import Image
+
+#とりあえずPILに頼ったらなんとかなるっぽい
+img = Image.open('ダウンロード.jpg')
+cvimage = np.asarray(img)
+
+# Numpy
+import cv2
+import numpy as np
+
+img_array = np.fromfile("ダウンロード.jpg", dtype=np.uint8)
+img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+```
