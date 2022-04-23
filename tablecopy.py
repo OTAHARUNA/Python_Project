2c28aa6d1249608db6d1ac319513d2bef03fe205
OTAHARUNA <radwimps.love867@gmail.com>
2022-04-23 20:43:37 +0900

    不要削除

--- tablecopy.py
+++ tablecopy.py
@@ -1,6 +1,5 @@
 # coding:utf-8
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import config as cf
 from bs4 import BeautifulSoup  # BeautifulSoup をインポート
